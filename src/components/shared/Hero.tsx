149b7919f02532e4136355a70a0eba728831e124
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2025-02-02 18:57:44 +0100

    `Refactor Hero component in Hero.tsx and Hero/index.tsx to use shared Hero component`

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -46,6 +46,37 @@ const Hero: React.FC<{
         className="object-cover w-full h-auto"
         onLoad={updateDimensions}
       />
+      <div className="absolute text-center flex flex-col space-y-20 md:gap-5 top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 z-[9999] container ">
+        <motion.div
+          initial={{ y: "-100vh", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            delay: 0.8,
+            duration: 1,
+            bounce: 0.5,
+            type: "spring",
+          }}
+        >
+          <div className="flex justify-center text-white items-center gap-5">
+            <span className="font-light">Home</span> <ChevronRight />{" "}
+            <span className="font-medium">{page}</span>
+            {slug && (
+              <React.Fragment>
+                <ChevronRight /> <span className="font-medium">{subPage}</span>
+              </React.Fragment>
+            )}
+          </div>
+          <h1 className="text-4xl mt-5 md:mt-0 font-[Playfair Display] relative md:text-5xl lg:text-6xl xl:text-9xl leading-[1.1!important] text-white font-bold">
+            <span className="">
+              <StyledFonts
+                fontFamily="Playfair Display"
+                text={caption}
+                color="#fff"
+              />
+            </span>
+          </h1>
+        </motion.div>
+      </div>
       {!loading && (
         <div
           className="absolute bg-[rgba(0,0,0,0.35)]"
@@ -55,40 +86,7 @@ const Hero: React.FC<{
             width: `${dimensions.width}px`,
             height: `${dimensions.height}px`,
           }}
-        >
-          <div className="absolute text-center flex flex-col space-y-20 md:gap-5 top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2  container ">
-            <motion.div
-              initial={{ y: "-100vh", opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{
-                delay: 0.8,
-                duration: 1,
-                bounce: 0.5,
-                type: "spring",
-              }}
-            >
-              <div className="flex justify-center text-white items-center gap-5">
-                <span className="font-light">Home</span> <ChevronRight />{" "}
-                <span className="font-medium">{page}</span>
-                {slug && (
-                  <React.Fragment>
-                    <ChevronRight />{" "}
-                    <span className="font-medium">{subPage}</span>
-                  </React.Fragment>
-                )}
-              </div>
-              <h1 className="text-4xl mt-5 md:mt-0 font-[Playfair Display] relative md:text-5xl lg:text-6xl xl:text-9xl leading-[1.1!important] text-white font-bold">
-                <span className="">
-                  <StyledFonts
-                    fontFamily="Playfair Display"
-                    text={caption}
-                    color="#fff"
-                  />
-                </span>
-              </h1>
-            </motion.div>
-          </div>
-        </div>
+        ></div>
       )}
     </section>
   );
